c4bf12cbd214770b9eb8faeb76583457e5f870c3
brajendra kumar <brajendrak0068@gmail.com>
2021-06-25 00:17:34 +0530

    updated recording controller

--- src/modules/statistics/LocalTracksDuration.js
+++ src/modules/statistics/LocalTracksDuration.js
@@ -141,7 +141,7 @@ export default class LocalTracksDuration {
         conference.addEventListener(JitsiConferenceEvents.TRACK_UPDATED, ()=>{
             const { localTracksDuration } = FINAL_STATE;
 
-            if (localTracksDuration.conference.startedTime === -1 || action.mediaType === 'presenter') {
+            if (localTracksDuration.conference.startedTime === -1) {
                 return;
             }
             FINAL_STATE = {
@@ -156,7 +156,7 @@ export default class LocalTracksDuration {
 
             const { localTracksDuration } = FINAL_STATE;
 
-            if (localTracksDuration.conference.startedTime === -1 || action.mediaType === 'presenter') {
+            if (localTracksDuration.conference.startedTime === -1) {
                 // We don't want to track the media duration if the conference is not joined yet because otherwise we won't
                 // be able to compare them with the conference duration (from conference join to conference will leave).
                 // Also, do not track media duration for presenter tracks.
