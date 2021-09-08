f1ff5aa0f92db1d0ff70e14e811d93061995da20
brajendra kumar <brajendrak0068@gmail.com>
2021-09-08 20:06:03 +0530

    jibri related changes

--- src/modules/recording/JibriSession.js
+++ src/modules/recording/JibriSession.js
@@ -223,7 +223,7 @@ export default class JibriSession {
             appData = {};
         }
         
-        appData["baseUrl"] =  window.location.origin;
+        appData["base_url"] =  window.location.origin;
         appData["token"] =  this._connection.emuc.xmpp.token;
         
         return $iq({
