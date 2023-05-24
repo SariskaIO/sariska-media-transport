58773cbba074b26995c2c8bc0dfb186dd52ca6b4
brajendrak00068 <brajendrak00068>
2023-05-24 12:46:26 +0530

    added more missing files

---
+++ dist/esm/modules/e2ee/E2EEErrors.js
@@ -0,0 +1,9 @@
+export var E2EEErrors;
+(function (E2EEErrors) {
+    E2EEErrors["E2EE_SAS_KEYS_MAC_MISMATCH"] = "e2ee.sas.keys-mac-mismatch";
+    E2EEErrors["E2EE_SAS_MAC_MISMATCH"] = "e2ee.sas.mac-mismatch";
+    E2EEErrors["E2EE_SAS_MISSING_KEY"] = "e2ee.sas.missing-key";
+    E2EEErrors["E2EE_SAS_COMMITMENT_MISMATCHED"] = "e2ee.sas.commitment-mismatched";
+    E2EEErrors["E2EE_SAS_CHANNEL_VERIFICATION_FAILED"] = "e2ee.sas.channel-verification-failed";
+    E2EEErrors["E2EE_SAS_INVALID_SAS_VERIFICATION"] = "e2ee.sas.invalid-sas-verification";
+})(E2EEErrors || (E2EEErrors = {}));
