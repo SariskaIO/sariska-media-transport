392477bc2cc28af0bceb3d384802848a2ea49641
dipak140 <dipak@sariska.io>
2023-09-18 12:22:39 +0530

    Send Owner id and app id in payload

--- dist/esm/modules/statistics/RTPStatsCollector.js
+++ dist/esm/modules/statistics/RTPStatsCollector.js
@@ -190,14 +190,27 @@ StatsCollector.prototype.errorCallback = function (error) {
 /**
  * Starts stats updates.
  */
+function parseJwt(token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+}
 function postDataToPricingService(authToken, roomName, payload, userControls, pricingServiceUrl) {
     let meeting_id = isDev ? `${roomName}@muc.dev.sariska.io` : `${roomName}@muc.sariska.io`;
     let url = pricingServiceUrl + meeting_id + "/usage";
     console.log("pricing service url:", pricingServiceUrl + meeting_id + "/usage");
+    decodedToken = parseJwt(authToken);
+    let owner_id = parseInt(decodedToken.context.group);
+    let app_id = decodedToken.sub;
     const api_payload = {
         meetingId: meeting_id,
         usage_type: 'MEDIA',
-        usage_payload: payload
+        usage_payload: payload,
+        owner_id: owner_id,
+        app_id: app_id
     };
     const headers = {
         "Authorization": "Bearer " + authToken,
